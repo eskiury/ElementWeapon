a8e7c357443994ca21b9db50f4e4bbc5b9686c53
eskiury <javierem2003@gmail.com>
2026-06-26 15:09:14 +0200

    Base estructurada Unreal

---
+++ Source/ElementWeapon/ElementWeapon.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ElementWeapon.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ElementWeapon, "ElementWeapon" );
